6905eb94383aec5765715da4e595bbe45ed1c0ed
Dominic Robinson <dominic.robinson@digital.justice.gov.uk>
2020-11-25 12:11:09 +0000

    DSO-648: added tags and supporting scripts

---
+++ tags/generate-from-existing-tags.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+# only saves the tags that are not currently defined elsewhere
+# i.e. would otherwise be deleted in non-incremental mode
+MODE=DEL
+
+# save all tags, even if they are already defined
+# MODE=all
+
+dir=$1
+shift 1
+tags=$@
+
+if [[ -z "$dir" ]]; then
+  echo "Usage: $0 <directory> [<tag1> .. <tagN>]"
+  exit 1
+fi
+
+if [[ ! -x ./aztagscli-helper.sh ]]; then
+  echo "Couldn't find aztagscli-helper.sh"
+  exit 1
+fi
+
+filename=$dir/tags
+
+if [[ -z $tags ]]; then
+  savetags=""
+else
+  savetags="--savetags $tags"
+
+  for tag in $tags; do
+    filename=${filename}.${tag}
+  done
+  filename=${filename}.txt
+fi
+
+./aztagscli-helper.sh $dir --changetypes "all" -v --savetagsfile "$filename" $savetags --
+
