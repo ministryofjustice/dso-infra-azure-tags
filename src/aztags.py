2b27afa7ca7e3631acac5ec3b3fef994b6fa5a39
reallydontask <agoney.garcia-deniz@digital.justice.gov.uk>
2021-03-19 12:16:31 +0000

    :cry:

--- src/aztags.py
+++ src/aztags.py
@@ -707,7 +707,6 @@ class AzTags:
         given subscription.  Compare the input CSVs with the actual resources
         and add discrepencies into the update list
         """
-                
         # pivot indexed on resource_id with the given tag values as a columns
         tagpivot = pandas.pivot_table(
             df[df.x_sub_id == sub_id],
